07c0bf49a3123d9ebbdbcb6d8fff077442aa2c73
Yadasko <v.citera@gmail.com>
2018-02-09 16:56:33 +0100

    List is now clickable
    Dirty way to do it though. Serves as an example.

--- app/js/testListe.js
+++ app/js/testListe.js
@@ -4,13 +4,27 @@ function illuminate(index) {
 	if (index > 0) $("#nb"+(index - 1)).removeClass("active");
 }
 
-
-let liste = " <ul class=\"list-group\" id=\"list\">";
+let liste = " <ul class=\"list-group\" class='liste' id=\"list\">";
 for (let i = 0; i < event.cue_list.length; i++) {
 	let cue = event.cue_list[i];
-	liste += "<li class=\"list-group-item\" id=\"nb" + i + "\">Note: "+cue.options.note+" - Vélocité: "+cue.options.velocity+
-		" - Channel: "+cue.channel+"</li>"
+	liste += "<li onclick=\"return toggle('hidden"+i+"')\" class=\"list-group-item\" id=\"nb" + i + "\">Note: "+cue.options.note
+	liste += "<p class=\"hidden\" id=\"hidden"+i+"\"> Vélocité: "+cue.options.velocity+" - Channel: "+cue.channel
+	liste += " - Délai: " + cue.delay
+	liste += "</p></li>"
 }
 liste += "</ul>"
 
 $("#list").html(liste);
+
+/*jQuery('#nb1').click(function() {
+	alert("test");
+});*/
+
+function toggle(a)
+{
+    let e = document.getElementById(a);
+    if(e.style.display == "block")
+        e.style.display = "none";
+    else
+        e.style.display = "block";
+}
