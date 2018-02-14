51deb7541917dea8047e09e554598de6a33f976c
keryann <keke2612@hotmail.fr>
2018-02-14 17:04:10 +0100

    Modifs menus

--- main.js
+++ main.js
@@ -32,6 +32,8 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+    require('./mainmenu') //Load file to change menu-bar
 }
 
 // This method will be called when Electron has finished
