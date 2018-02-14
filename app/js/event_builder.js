043fbcc17ea46292d9aeb0b061e8ed1fdb7432f7
Arthur LE SAINT <arthur.lesaint@hotmail.fr>
2018-02-14 18:52:19 +0100

    update_cue_delay function

--- app/js/event_builder.js
+++ app/js/event_builder.js
@@ -22,6 +22,29 @@ function create_cue(parent_cue_list, index, type, channel, delay, options) {
 	parent_cue_list.splice(index, 0, cue);
 }
 
+
+function update_cue_delay(parent_cue_list, index_cue, new_delay) {
+	var old_delay = parent_cue_list[index_cue].delay;
+	parent_cue_list[index_cue].delay = new_delay;
+
+	for (var i = 0; i < parent_cue_list.length; i++) {
+		if(parent_cue_list[i].delay <= new_delay && parent_cue_list[i+1].delay >= new_delay){
+			if (new_delay > old_delay) {
+				move_cue(parent_cue_list, index_cue, i);
+			}
+			else move_cue(parent_cue_list, index_cue, i+1);
+			
+			break;
+		}
+	}
+}
+
+function move_cue(parent_cue_list, old_index, new_index) {
+	// arr.splice(old_index, 1)[0] -> removes the cue from the list and give us the cue in return
+	parent_cue_list.splice(new_index, 0, parent_cue_list.splice(old_index, 1)[0]);
+}
+
+
 /*
 * Returns an empty event
 params:
@@ -38,3 +61,4 @@ function create_event(id, name, options) {
 	}
 	return event;
 }
+
